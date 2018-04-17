dbaac3f9811ee2a6edc4dd6f4d3d0ee7eb43ae0a
Jamie <jamie@Jamies-MacBook-Pro.local>
2018-04-18 01:39:57 +0200

    improved taxonomy map

--- src/js/components/taxonomy-layout.js
+++ src/js/components/taxonomy-layout.js
@@ -35,26 +35,40 @@
 		function makeTree(d,div){
 			d.tree.forEach(function(child){
 					var containerDiv = el('div.modal-taxonomy-map-ul-head-container',[])
-					div.appendChild( getActiveList(child, containerDiv,12,.7) )
+					div.appendChild( getActiveList(child, containerDiv,12,1,1) )
 				})
 			return div
 		}
 
-		function getActiveList(d, ul, font, sqr){
+		function getActiveList(d, ul, font, sqr,depth){
+			//used to shrink size of square beside text,font size and padding for Div
+			sqr = sqr-0.065>0 ? sqr-0.065:sqr
+			font = font-0.25>0 ? font-0.25:font
 			var tree = d.tree
 			var title = d.short.length<15? d.short: d.short.substring(0,13)+'...'
 			var span = el('span.modal-flex-span', [title.toLowerCase()])
 			span.style.fontSize = font +'px'
 			var li = el('li',[span])
+			//create squares
 			var square = el('div.small-square',[li])
 			square.style.background = color(d.id())(relativeUse(d))
 			square.style.height=sqr +"em"
 			square.style.width=sqr +"em"
+			square.style.marginTop = depth <10? 8+(depth/2) + '%' : 11 + (depth/2) + '%' 
+			//create outline grid
+			var mark = depth >1? '¦':''
+			var lines = mark + Array(depth).join('-')
+			var lineSpan= el('span.modal-outline-span', [lines])	
+			var lineLi = el('li',[lineSpan])
+			var outlineDiv = el('div.modal-taxonomy-map-outLineContainer',[lineLi])
+			
 			var containerDiv = el('div.modal-taxonomy-map-ul-container',[square,li])
-			ul.appendChild(containerDiv)
+			var outerContainer = el('div.modal-taxonomy-map-ul-container',[outlineDiv,containerDiv])
+			var ulTaxMap = el('ul.ul-taxonomy-map',[])
+			ul.appendChild(outerContainer)
 			if(typeof tree !== 'undefined' && tree.length >0){
 				tree.forEach(function(child){
-					ul.appendChild( getActiveList(child, el('ul.ul-taxonomy-map',[]),font-.25,sqr-0.065) )
+					ul.appendChild( getActiveList(child, ulTaxMap,font,sqr,depth+1) )
 				})
 			}
 			return ul
