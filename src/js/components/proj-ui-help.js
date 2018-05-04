6859d03d2c86ad5e44dfb95403c8a10811cfcc3a
Jamie <jamie@Jamies-MacBook-Pro.local>
2018-05-04 11:40:30 +0200

    go

--- src/js/components/proj-ui-help.js
+++ src/js/components/proj-ui-help.js
@@ -14,23 +14,13 @@
 
     G.projUIHelp = function() {
         var p = "Click on the facets of the wheel on the right and extend the taxonomy out from the selected facet. You need to input a unique short name,a long name and description. \n "
-        var title = el("h3", ["Taxonomy Legend"])
-        var baseLegend = el("a#baseLegend.proj-ui-taxonomy-ext-legend", [])
-        var a_baseLegend = el("a", ["Base Taxonomy"])
-        var extLegend = el("a#extendedLegend.proj-ui-taxonomy-ext-legend", [])
-        var a_extLegend =el("a", ["Extended Taxonomy"])
-        var div1 = el("div.divider-wrapper", [ baseLegend,a_baseLegend])
-        var div2 = el("div.divider-wrapper", [extLegend,a_extLegend])
-        var ovContainer  = el("div.ov-tax-container", [title,div1,div2])
-        var mainContainer = el("div.collections-taxonomy-ext-legend-container", [ovContainer])
-
+     
         var modal = el('div#modalHelp.modal.confirm', [
             el('div', [
                 window.modals.closeButton(),
                 el("h1.text-title", ['How To Use']),
                 el('div.modal-divider'),
                 p,
-                mainContainer,
                 el("div#bottom-divider.modal-divider"),
                 window.modals.cancelButton()
             ])
--- src/js/components/taxonomy-layout.js
+++ src/js/components/taxonomy-layout.js
@@ -34,19 +34,19 @@
 
 		function makeTree(d,div){
 			d.tree.forEach(function(child){
-					var containerDiv = el('div.modal-taxonomy-map-ul-head-container',[])
-					div.appendChild( getActiveList(child, containerDiv,12,1,1) )
-				})
+				var containerDiv = el('div.modal-taxonomy-map-ul-head-container',[])
+				div.appendChild( getActiveList(child, containerDiv,12,1,1) )
+			})
 			return div
 		}
 
 		function getActiveList(d, ul, font, sqr,depth){
 			//used to shrink size of square beside text,font size and padding for Div
-			sqr = sqr-0.065>0 ? sqr-0.065:sqr
+			sqr = sqr-0.065>0.25 ? sqr-0.065:sqr
 			font = font-0.25>0 ? font-0.25:font
 			var tree = d.tree
 			var title = d.short.length<15? d.short: d.short.substring(0,13)+'...'
-			var span = el('span.modal-flex-span', [title.toLowerCase()])
+			var span = el('span#mapText'+d.short+'.modal-flex-span', [title.toLowerCase()])
 			span.style.fontSize = font +'px'
 			var li = el('li',[span])
 			//create squares
@@ -58,12 +58,16 @@
 			//create outline grid
 			var mark = depth >1? '¦':''
 			var lines = mark + Array(depth).join('-')
-			var lineSpan= el('span.modal-outline-span', [lines])	
+			var lineSpan= el('span#map'+d.short+'.modal-outline-span', [lines])	
 			var lineLi = el('li',[lineSpan])
 			var outlineDiv = el('div.modal-taxonomy-map-outLineContainer',[lineLi])
 			
 			var containerDiv = el('div.modal-taxonomy-map-ul-container',[square,li])
 			var outerContainer = el('div.modal-taxonomy-map-ul-container',[outlineDiv,containerDiv])
+			outerContainer.addEventListener('click', function(){
+				highlightMap(d)
+			})
+
 			var ulTaxMap = el('ul.ul-taxonomy-map',[])
 			ul.appendChild(outerContainer)
 			if(typeof tree !== 'undefined' && tree.length >0){
@@ -74,10 +78,44 @@
 			return ul
 		}
 
+		function highlightMap(d){
+			resetMap(serp.dfs('root'))
+			var current = d.short
+			var parent = d.parent
+			var x = document.getElementById('map'+current)
+			var y = document.getElementById('mapText'+current)
+			x.classList.toggle("taxonomy-map-highlight")
+			y.classList.toggle("taxonomy-map-text-highlight")
+			while(parent!='root'){
+				var next= serp.dfs(parent)
+				current = next.short
+				parent = next.parent
+				x = document.getElementById('map'+current)
+				x.classList.toggle("taxonomy-map-highlight")
+				y = document.getElementById('mapText'+current)
+				y.classList.toggle("taxonomy-map-text-highlight")
+			}
+		}
+		function resetMap(d){
+			var tree = d.tree
+			if(typeof tree !== 'undefined' && tree.length >0){
+				tree.forEach(function(child){
+					resetMap(child)
+				})
+			}
+			if(d.short !== 'root'){
+				var x = document.getElementById('map'+d.short)
+				x.classList.remove("taxonomy-map-highlight")
+				var y = document.getElementById('mapText'+d.short)
+				y.classList.remove("taxonomy-map-text-highlight")
+			}
+			return
+		}
+
 		var div = el('div.modal-flex',[])
 
 		var modal = el('div.modal', [
-			el('div', [
+			el('div#modalContainer', [
 				window.modals.closeButton(),
 				el("h1.text-title", ['Taxonomy Tree']),
 				el("div.modal-divider"),
@@ -86,9 +124,9 @@
 			])
 		])
 		return new Promise(function (F, R) {
-			
 			document.body.appendChild(modal)
 			window.modals.appear(modal)
+			document.getElementById('modalContainer').style.maxWidth='850px'
 		})
 	}
 })(window.components || (window.components = {}));
